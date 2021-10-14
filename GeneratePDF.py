0f47b58985a056d9960e0121e383096100284f3e
GautamKumar <gautamajay52@gmail.com>
2021-10-14 13:00:57 +0530

    code refactoring and ...

--- GeneratePDF.py
+++ GeneratePDF.py
@@ -1,72 +1,72 @@
-def drawMyRuler(pdf):
-    pdf.drawString(100,810, 'x100')
-    pdf.drawString(200,810, 'x200')
-    pdf.drawString(300,810, 'x300')
-    pdf.drawString(400,810, 'x400')
-    pdf.drawString(500,810, 'x500')
+from reportlab.pdfgen import canvas
 
-    pdf.drawString(10,100, 'y100')
-    pdf.drawString(10,200, 'y200')
-    pdf.drawString(10,300, 'y300')
-    pdf.drawString(10,400, 'y400')
-    pdf.drawString(10,500, 'y500')
-    pdf.drawString(10,600, 'y600')
-    pdf.drawString(10,700, 'y700')
-    pdf.drawString(10,800, 'y800')    
-
-
-from reportlab.pdfgen import canvas 
-
-'''Font Setup'''
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfbase import pdfmetrics
+"""Font Setup"""
 from reportlab.lib import colors
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
-class WhatsAppReport():
 
+def drawMyRuler(pdf):
+    pdf.drawString(100, 810, "x100")
+    pdf.drawString(200, 810, "x200")
+    pdf.drawString(300, 810, "x300")
+    pdf.drawString(400, 810, "x400")
+    pdf.drawString(500, 810, "x500")
+
+    pdf.drawString(10, 100, "y100")
+    pdf.drawString(10, 200, "y200")
+    pdf.drawString(10, 300, "y300")
+    pdf.drawString(10, 400, "y400")
+    pdf.drawString(10, 500, "y500")
+    pdf.drawString(10, 600, "y600")
+    pdf.drawString(10, 700, "y700")
+    pdf.drawString(10, 800, "y800")
+
+
+class WhatsAppReport:
     def __init__(self, data):
-        self.fileName = f'WhatsApp-Chat-Report.pdf'
-        self.documentTitle = 'WhatsApp Chat Report'
-        self.title = 'Chat-Insights'
-        self.subTitle = f'Your WhatsApp Chat Report'
-        self.image = 'message-graph.png'
-        
-        '''Chat Stats'''
-        self.totalMessages = data['totalMessages']
-        self.totalDays = data['totalDays']
-        self.averageTextPerDay = data['averageTextPerDay']
-        self.maxTextOneDay = data['maxTextOneDay']
-        self.maxTextDate = data['maxTextDate']
-        
+        self.fileName = f"WhatsApp-Chat-Report.pdf"
+        self.documentTitle = "WhatsApp Chat Report"
+        self.title = "Chat-Insights"
+        self.subTitle = f"Your WhatsApp Chat Report"
+        self.image = "message-graph.png"
+
+        """Chat Stats"""
+        self.totalMessages = data["totalMessages"]
+        self.totalDays = data["totalDays"]
+        self.averageTextPerDay = data["averageTextPerDay"]
+        self.maxTextOneDay = data["maxTextOneDay"]
+        self.maxTextDate = data["maxTextDate"]
+
         self.pdf = canvas.Canvas(self.fileName)
         self.pdf.setTitle(self.documentTitle)
 
     def generate(self):
-        '''Draw some lines'''
+        """Draw some lines"""
         self.pdf.line(30, 750, 550, 750)
         self.pdf.line(130, 690, 450, 690)
         self.pdf.line(130, 540, 450, 540)
 
-        '''The Title'''
+        """The Title"""
         self.pdf.setFont("Helvetica", 36)
         self.pdf.drawCentredString(300, 770, self.title)
-        
-        '''The Subtitle'''
+
+        """The Subtitle"""
         self.pdf.setFillColorRGB(0, 0, 255)
         self.pdf.setFont("Courier-Bold", 24)
-        self.pdf.drawCentredString(290,710, self.subTitle)
-        
-        textLines = [
-        f'You and your friend have been talking on WhatsApp for {self.totalDays} days.',
-        f'You guys have exchanged a total of {self.totalMessages} text messages till date!',
-        f'On an average, {self.averageTextPerDay} text messages have been exchanged per day.',
-        f'{self.maxTextDate} was the day you achieved a record high of text messages',
-        f'exchanged in one single day.',
-        f'Your shared record for most number of text messages sent in one day ',
-        f'lies at {self.maxTextOneDay}. Keep up the good work!',
-]
+        self.pdf.drawCentredString(290, 710, self.subTitle)
 
-        '''The numbers'''
+        textLines = [
+            f"You and your friend have been talking on WhatsApp for {self.totalDays} days.",
+            f"You guys have exchanged a total of {self.totalMessages} text messages till date!",
+            f"On an average, {self.averageTextPerDay} text messages have been exchanged per day.",
+            f"{self.maxTextDate} was the day you achieved a record high of text messages",
+            f"exchanged in one single day.",
+            f"Your shared record for most number of text messages sent in one day ",
+            f"lies at {self.maxTextOneDay}. Keep up the good work!",
+        ]
+
+        """The numbers"""
         text = self.pdf.beginText(40, 660)
         text.setFont("Courier", 13)
         text.setFillColor(colors.black)
@@ -75,23 +75,9 @@ class WhatsAppReport():
             text.textLine(line)
         self.pdf.drawText(text)
 
-        '''The Graph'''
+        """The Graph"""
         self.pdf.drawInlineImage(self.image, -1, 50)
         self.pdf.save()
 
 
-
-
-
-
 # drawMyRuler(pdf)
-
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,61 +1,89 @@
+import glob
+import re
+from collections import defaultdict
+from typing import Any, Dict, List
+
+import matplotlib.pyplot as plt
+
 from GeneratePDF import WhatsAppReport
 
-file_name = '''chat.txt'''
-chat = open(file_name,'r',encoding="utf-8")
-chats = chat.readlines()
-user1 = {}
-
-import matplotlib.pyplot as plt 
-for session in chats:
-	try:
-		int(session.split(",")[0].split("/")[0])
-		int(session.split(",")[0].split("/")[1])
-		if session.split(",")[0] not in user1:
-			user1[session.split(",")[0]] = 1
-		else:
-			user1[session.split(",")[0]]=1+user1[session.split(",")[0]] 
-	except:
-		pass
-x = []
-y = []
-j = 0
-totalMessages = 0
-
-for i in user1:
-	y.append(j)
-	x.append(user1[i])
-	totalMessages = totalMessages + user1[i] 
-	j+=1
-
-Keymax = max(user1, key=user1.get) 
-fig = plt.figure()
-# fig.suptitle(file_name, fontsize=14, fontweight='bold')
-ax = fig.add_subplot(111)
-fig.subplots_adjust(top=0.85)
+MESSAGE_PATTERN = re.compile(
+    r"(\d+\/\d+\/\d+),\s.*-\s([^:]+):\s(.*)"
+)  # date, sender, message of actual messages, not whatsapp events
 
 
-#ax.set_title('total number of messages are {.} in {.} days. Average {.} text/day. On {} max number of text were sent:{}'.format(totalMessages,totalDays,totalMessages//len(user1),Keymax,user1[Keymax]))
+class WhatsAppAnalyzer:
+    def __init__(self) -> None:
+        self.file_name = glob.glob("./WhatsApp*.txt")
+        self.x = []
+        self.y = []
+        self.totalMessages = 0
 
-ax.plot(y,x)
-ax.set_xlabel('days') 
-ax.set_ylabel('number of messages') 
+    def chats_(self) -> List[str]:
+        chat = open(self.file_name[0], "r", encoding="utf-8")
+        chats = chat.readlines()
+        chat.close()
+        return chats
 
-data = {"totalMessages" : totalMessages,
-		"totalDays" : len(user1),
-		"averageTextPerDay" : totalMessages//len(user1),
-		"maxTextDate" : Keymax,
-		"maxTextOneDay" : user1[Keymax]
-}
+    def parse_info(self) -> Dict[str, int]:
+        """Convert the text file into a dictionary of dates and number of messages"""
 
-# plt.show()
-# D = user1
-# plt.bar(range(len(D)), list(D.values()), align='center')
-# plt.xticks(range(len(D)), list(D.keys()))
-#plt.get_current_fig_manager().full_screen_toggle() # toggle fullscreen mode
-#plt.show()
+        data = defaultdict(lambda: 0)
+        chats = self.chats_()
+        for line in chats:
+            a = re.findall(MESSAGE_PATTERN, line)
+            if a:
+                date, _, _ = a[0]
+                data[date] += 1
+        return data
 
-plt.savefig('message-graph.png')
-#plt.close()
+    def plot_info(self) -> Dict[str, Any]:
+        n = 0
+        data = self.parse_info()
+        for i in data:
+            self.y.append(n)
+            self.x.append(data[i])
+            self.totalMessages += data[i]
+            n += 1
 
-report = WhatsAppReport(data)
-report.generate()
+        Keymax = max(data, key=data.get)
+
+        # fig = plt.figure()
+        # fig.suptitle(file_name, fontsize=14, fontweight='bold')
+        # ax = fig.add_subplot(111)
+        fig, ax = plt.subplots(1, 1)
+        plt.subplots_adjust(top=0.85)
+
+        # ax.set_title('total number of messages are {.} in {.} days. Average {.} text/day. On {} max number of text were sent:{}'.format(totalMessages,totalDays,totalMessages//len(data),Keymax,data[Keymax]))
+        ax.plot(self.y, self.x)
+        ax.set_xlabel("days$ \longrightarrow $")
+        ax.set_ylabel("number of messages$ \longrightarrow $")
+
+        data = {
+            "totalMessages": self.totalMessages,
+            "totalDays": len(data),
+            "averageTextPerDay": self.totalMessages // len(data),
+            "maxTextDate": Keymax,
+            "maxTextOneDay": data[Keymax],
+        }
+        # print(data)
+        # plt.show()
+        # D = data
+        # plt.bar(range(len(D)), list(D.values()), align='center')
+        # plt.xticks(range(len(D)), list(D.keys()))
+        # plt.get_current_fig_manager().full_screen_toggle() # toggle fullscreen mode
+        # plt.show()
+
+        plt.savefig("message-graph.png")
+        # plt.close()
+        return data
+
+    def generate_pdf(self) -> None:
+        data = self.plot_info()
+        report = WhatsAppReport(data)
+        report.generate()
+
+
+if __name__ == "__main__":
+    wa = WhatsAppAnalyzer()
+    wa.generate_pdf()
